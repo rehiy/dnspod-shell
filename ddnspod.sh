5fc71f3581b42b0fac21fd78486aeceffd6c6d06
Jiaqiu Jiang <jjqzy@foxmail.com>
2021-02-08 19:20:13 +0800

    add ipv6 support & api call improvements

--- ddnspod.sh
+++ ddnspod.sh
@@ -9,4 +9,7 @@ arToken="12345,7676f344eaeaea9074c123451234512d"
 
 # Place each domain you want to check as follows
 # you can have multiple arDdnsCheck blocks
+# add 4 or 6 to tail will specify the IP version used, default 4
+# IPv6:
+#   arDdnsCheck "test.org" "subdomain6" 6
 arDdnsCheck "test.org" "subdomain"
