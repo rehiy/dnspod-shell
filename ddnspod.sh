3e7ccc78bd2f9f0c8997168dabccd98c11391715
若海 <mail@anrip.com>
2021-03-03 09:19:42 +0800

    优化部分判断逻辑

--- ddnspod.sh
+++ ddnspod.sh
@@ -9,7 +9,9 @@ arToken="12345,7676f344eaeaea9074c123451234512d"
 
 # Place each domain you want to check as follows
 # you can have multiple arDdnsCheck blocks
-# add 4 or 6 to tail will specify the IP version used, default 4
+
 # IPv6:
-#   arDdnsCheck "test.org" "subdomain6" 6
+# arDdnsCheck "test.org" "subdomain" 6
+
+# IPv4:
 arDdnsCheck "test.org" "subdomain"
