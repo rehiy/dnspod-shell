d7061aa28fdad8e2f71bb76f6917a7842398b0c6
Howardnm <850996480@qq.com>
2022-07-06 23:17:05 +0800

    1

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+rm -f ardnspod
+rm -f ddnspod.sh
+rm -f myddns.sh
+curl https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/ardnspod
+curl https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/ddnspod.sh
+curl https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/myddns.sh
