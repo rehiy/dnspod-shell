60aede07f69b7b022af6d17ddd78e6f83fd37271
Howardnm <55622355+Howardnm@users.noreply.github.com>
2022-07-07 01:25:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 #
 mkdir /root/myddns
 rm -f /root/myddns/*
-wget -P /root/myddns/ https://ghproxy.com/https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/ardnspod
-wget -P /root/myddns/ https://ghproxy.com/https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/ddnspod.sh
-wget -P /root/myddns/ https://ghproxy.com/https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/myddns.sh
+wget -P /root/myddns/ https://cdn.jsdelivr.net/gh/Howardnm/dnspod-shell@master/ardnspod
+wget -P /root/myddns/ https://cdn.jsdelivr.net/gh/Howardnm/dnspod-shell@master/ddnspod.sh
+wget -P /root/myddns/ https://cdn.jsdelivr.net/gh/Howardnm/dnspod-shell@master/myddns.sh
 echo "安装完成"
 rm -- "$0"
