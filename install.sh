e760446ca2580b793028bc9623ff15a10256400e
Howardnm <850996480@qq.com>
2022-07-07 01:22:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ rm -f /root/myddns/*
 wget -P /root/myddns/ https://ghproxy.com/https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/ardnspod
 wget -P /root/myddns/ https://ghproxy.com/https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/ddnspod.sh
 wget -P /root/myddns/ https://ghproxy.com/https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/myddns.sh
+echo "安装完成"
 rm -- "$0"
