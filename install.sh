d602e62128b2eec9cbb0e2a1067d994e38365d5c
Howardnm <850996480@qq.com>
2022-07-06 23:23:11 +0800

    1

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ rm -f /root/myddns/*
 wget -P /root/myddns/ https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/ardnspod
 wget -P /root/myddns/ https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/ddnspod.sh
 wget -P /root/myddns/ https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/myddns.sh
+rm -- "$0"
