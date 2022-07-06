cd32fc65c2caf4851d066adbf8653db1232f1771
Howardnm <850996480@qq.com>
2022-07-06 23:43:02 +0800

    1

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 #
 mkdir /root/myddns
 rm -f /root/myddns/*
-wget -P /root/myddns/ https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/ardnspod
-wget -P /root/myddns/ https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/ddnspod.sh
-wget -P /root/myddns/ https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/myddns.sh
+wget -P /root/myddns/ https://ghproxy.com/https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/ardnspod
+wget -P /root/myddns/ https://ghproxy.com/https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/ddnspod.sh
+wget -P /root/myddns/ https://ghproxy.com/https://raw.githubusercontent.com/Howardnm/dnspod-shell/master/myddns.sh
 rm -- "$0"
