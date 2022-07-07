96bf3e0c99dc857046d241fcf1f9e7204c1ce1f1
Howardnm <850996480@qq.com>
2022-07-07 09:41:15 +0800

    fix

--- ddnspod.sh
+++ ddnspod.sh
@@ -11,8 +11,8 @@ arToken="243177,7676f344eaeaea9074c123451234512d"
 # you can have multiple arDdnsCheck blocks
 
 # IPv4:
-# arDdnsCheck "test.org" "subdomain"
+arDdnsCheck "test.org" "subdomain"
 
 # IPv6:
-# arDdnsCheck "test.org" "subdomain6" 6
+arDdnsCheck "test.org" "subdomain6" 6
 #              "域名"   "子域(主机记录)"
