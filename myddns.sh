16d6786770a8e46a6fad4f0183d6049fee04a07c
Howardnm <55622355+Howardnm@users.noreply.github.com>
2022-07-07 09:27:02 +0800

    Update myddns.sh

--- myddns.sh
+++ myddns.sh
@@ -26,7 +26,7 @@ get_ipv6() {
     mac=$(cat mac.txt)
     awk "/${mac}/" 1.txt > 2.txt # 列出含有《45:52:82:9e:73:86》mac地址的行
     sed  "/fe80:/d" 2.txt > 1.txt # 删除含有《fe80:》的行
-    sed  "/192.168./d" 1.txt > 2.txt # 删除含有ipv4的行
+    sed  "/${HDCP_ip}/d" 1.txt > 2.txt # 删除含有ipv4的行
     awk '{print $1}' 2.txt > 1.txt # 保留第一列的ipv6
     mac_ipv6=$(sed -n 1p 1.txt) # 打印第一行的ipv6，赋值到变量
     echo " "
@@ -36,6 +36,9 @@ get_ipv6() {
 }
 
 ######----------######----------######----------######----------######----------######
+
+# dhcp服务分发的ip段(你的ip段是10.10.10.0/24  只填"10.10."前两个就行)
+HDCP_ip="192.168."
 
 # dnspod的API密钥 申请地址：https://console.dnspod.cn/account/token/token
 DNSpod_ID="243177"
@@ -58,3 +61,6 @@ mac="" # 设备mac地址
 domainU="" # 主域名
 subdomainU="" # 子域(主机记录)
 # run_dnspod # 运行程序
+
+# 域名解析ddns4
+# ...可以继续加
