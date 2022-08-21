0bc7d9df3b4e273978eff7b4a1cae014f1df5d73
Vigilans <vigilans@foxmail.com>
2022-08-21 10:25:22 +0800

    Distinguish record ip unchanged event from updated event

--- ddnspod.sh
+++ ddnspod.sh
@@ -7,6 +7,10 @@
 # Combine your token ID and token together as follows
 arToken="12345,7676f344eaeaea9074c123451234512d"
 
+# Return code when the last record IP is same as current host IP
+# Set this to a value other than 0 to distinguish with a successful ddns update
+# arErrCodeUnchanged=0
+
 # Place each domain you want to check as follows
 # you can have multiple arDdnsCheck blocks
 
