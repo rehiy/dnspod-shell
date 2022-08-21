36ab69bd59714357c9ec8dfebcbd2c91858bdc42
Vigilans <vigilans@foxmail.com>
2022-08-21 13:31:36 +0800

    Allow url for querying public ipv6 address to be configurable

--- ddnspod.sh
+++ ddnspod.sh
@@ -7,6 +7,10 @@
 # Combine your token ID and token together as follows
 arToken="12345,7676f344eaeaea9074c123451234512d"
 
+# Web endpoint to be used for querying the public IPv6 address
+# Set this to override the default url provided by ardnspod
+# arIp6QueryUrl="https://6.ipw.cn"
+
 # Return code when the last record IP is same as current host IP
 # Set this to a value other than 0 to distinguish with a successful ddns update
 # arErrCodeUnchanged=0
