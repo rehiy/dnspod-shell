593f40a88f337b225a11f34916b8f20085b0c278
若海 <wang@rehiy.com>
2023-06-03 17:33:07 +0800

    默认启用所有配置

--- ddnspod.sh
+++ ddnspod.sh
@@ -2,19 +2,27 @@
 #
 
 # Import ardnspod functions
+
 . /your_real_path/ardnspod
 
 # Combine your token ID and token together as follows
+
 arToken="12345,7676f344eaeaea9074c123451234512d"
 
 # Web endpoint to be used for querying the public IPv6 address
 # Set this to override the default url provided by ardnspod
-# arIp4QueryUrl="http://ipv4.rehi.org/ip"
-# arIp6QueryUrl="http://ipv6.rehi.org/ip"
+
+arIp4QueryUrl="http://ipv4.rehi.org/ip"
+arIp6QueryUrl="http://ipv6.rehi.org/ip"
+
+# The temp file to store the last record ip
+
+arLastRecordFile=/tmp/ardnspod_last_record
 
 # Return code when the last record IP is same as current host IP
 # Set this to a value other than 0 to distinguish with a successful ddns update
-# arErrCodeUnchanged=0
+
+arErrCodeUnchanged=0
 
 # Place each domain you want to check as follows
 # you can have multiple arDdnsCheck blocks
